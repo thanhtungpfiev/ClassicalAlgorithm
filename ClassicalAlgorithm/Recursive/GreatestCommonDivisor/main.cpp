f91ad3555c006e96c7aa0b8d46a929867e88f104
tung.dao <tung.dao@lge.com>
2022-05-05 16:55:19 +0700

    Greatest Common Divisor

---
+++ ClassicalAlgorithm/Recursive/GreatestCommonDivisor/main.cpp
@@ -0,0 +1,27 @@
+/**********************************************************************
+ *
+ * author vcs tung.dao@lge.com
+ *
+ * date 5/5/2022
+ *
+ * class %{Cpp:License:ClassName}
+ *
+ * brief
+ *
+***********************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+
+int main()
+{
+    cout << gcd(270, 192) << endl;
+    return 0;
+}
